19ee2a945ebb536d64ce52ddde06648eb92dc0f1
Paul Schweng <paul.schweng@siemens.com>
2023-05-29 20:42:15 +0200

    refactoring: moved and extracted methods in BodyData

--- 2-cleanproject-application/src/main/java/de/dhbw/cleanproject/application/bodydata/BodyDataApplicationService.java
+++ 2-cleanproject-application/src/main/java/de/dhbw/cleanproject/application/bodydata/BodyDataApplicationService.java
@@ -31,15 +31,9 @@ public class BodyDataApplicationService implements BodyDataApplication {
                     .bodyParts(new ArrayList<>())
                     .build();
 
-        for (BodyPartType type : BodyPartType.values()) {
-            // if this BodyPartType is dual, we have to handle right and left separately
-            if (type.isDual()) {
-                addOrUpdateMeasurement(bodyDataMeasurements, existingBodyData, type, BodySide.RIGHT);
-                addOrUpdateMeasurement(bodyDataMeasurements, existingBodyData, type, BodySide.LEFT);
-            } else {
-                addOrUpdateMeasurement(bodyDataMeasurements, existingBodyData, type, BodySide.NONE);
-            }
-        }
+        existingBodyData.addMeasurements(bodyDataMeasurements);
+
+
 
         // save the updated BodyData back to the database
         bodyDataRepository.save(existingBodyData);
@@ -50,39 +44,6 @@ public class BodyDataApplicationService implements BodyDataApplication {
     public BodyData getBodyData() {
         User principal = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return bodyDataRepository.getBodyData(principal.getId());
-    }
-
-    private void addOrUpdateMeasurement(BodyData bodyDataMeasurements, BodyData existingBodyData, BodyPartType type, BodySide side){
-        Optional<BodyPart> optionalNewBodyPart = bodyDataMeasurements.getBodyParts().stream()
-                .filter(bodyPart -> bodyPart.getType().equals(type) && bodyPart.getSide().equals(side))
-                .findFirst();
-
-
-        if (optionalNewBodyPart.isEmpty())
-            return;
-
-        BodyPart newBodyPart = optionalNewBodyPart.get();
-
-        // find the existing BodyPart in the existing BodyData's bodyParts list
-        Optional<BodyPart> optionalExistingBodyPart = existingBodyData.getBodyParts().stream()
-                .filter(bodyPart -> bodyPart.getType().equals(type) && bodyPart.getSide().equals(side))
-                .findFirst();
-
-        // if the existing BodyPart exists
-        if (optionalExistingBodyPart.isPresent()) {
-            BodyPart existingBodyPart = optionalExistingBodyPart.get();
-
-            if(existingBodyPart.getMeasurements().stream().anyMatch(measurement -> measurement.getDate().equals(newBodyPart.getMeasurements().get(0).getDate())))
-                throw new CustomException("Data already exists for this date. Try again tomorrow", "Could not add data!");
-
-            // add the new measurements to the existing BodyPart
-            existingBodyPart.getMeasurements().addAll(newBodyPart.getMeasurements());
-        } else {
-            // if the BodyPart does not exist in the existing BodyData
-            // add the new BodyPart to the existing BodyData's bodyParts list
-            existingBodyData.getBodyParts().add(newBodyPart);
-        }
-
     }
 
 
--- 3-cleanproject-domain/src/main/java/de/dhbw/cleanproject/domain/bodydata/BodyData.java
+++ 3-cleanproject-domain/src/main/java/de/dhbw/cleanproject/domain/bodydata/BodyData.java
@@ -1,5 +1,6 @@
 package de.dhbw.cleanproject.domain.bodydata;
 
+import de.dhbw.cleanproject.domain.exception.CustomException;
 import de.dhbw.cleanproject.domain.user.User;
 import lombok.*;
 import org.hibernate.annotations.Type;
@@ -7,6 +8,7 @@ import org.hibernate.annotations.Type;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @NoArgsConstructor
@@ -29,6 +31,58 @@ public class BodyData {
 
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     private List<BodyPart> bodyParts = new ArrayList<>();
+
+    public void addMeasurements(BodyData bodyDataMeasurements) {
+        for (BodyPartType type : BodyPartType.values()) {
+            // if this BodyPartType is dual, we have to handle right and left separately
+            if (type.isDual()) {
+                addMeasurement(bodyDataMeasurements, type, BodySide.RIGHT);
+                addMeasurement(bodyDataMeasurements, type, BodySide.LEFT);
+            } else {
+                addMeasurement(bodyDataMeasurements, type, BodySide.NONE);
+            }
+        }
+    }
+
+
+    private void addMeasurement(BodyData bodyDataMeasurements, BodyPartType type, BodySide side){
+        Optional<BodyPart> optionalNewBodyPart = bodyDataMeasurements.getBodyParts().stream()
+                .filter(bodyPart -> bodyPart.getType().equals(type) && bodyPart.getSide().equals(side))
+                .findFirst();
+
+        if (optionalNewBodyPart.isEmpty())
+            return;
+
+        BodyPart newBodyPart = optionalNewBodyPart.get();
+
+        // find the existing BodyPart in the existing BodyData's bodyParts list
+        Optional<BodyPart> optionalExistingBodyPart = this.getBodyParts().stream()
+                .filter(bodyPart -> bodyPart.getType().equals(type) && bodyPart.getSide().equals(side))
+                .findFirst();
+
+        // if the existing BodyPart exists
+        if (optionalExistingBodyPart.isPresent()) {
+            addMeasurementToExistingBodyPart(optionalExistingBodyPart.get(), newBodyPart);
+        } else {
+            // if the BodyPart does not exist in the existing BodyData
+            // add the new BodyPart to the existing BodyData's bodyParts list
+            this.getBodyParts().add(newBodyPart);
+        }
+
+    }
+
+    private void addMeasurementToExistingBodyPart(BodyPart existingBodyPart, BodyPart newBodyPart) {
+
+        boolean wasMeasurementAddedToday = existingBodyPart.getMeasurements().stream()
+                .anyMatch(measurement ->
+                        measurement.getDate().equals(newBodyPart.getMeasurements().get(0).getDate())
+                );
+        if(wasMeasurementAddedToday)
+            throw new CustomException("Data already exists for this date. Try again tomorrow", "Could not add data!");
+
+        // add the new measurements to the existing BodyPart
+        existingBodyPart.getMeasurements().addAll(newBodyPart.getMeasurements());
+    }
 
 
 
