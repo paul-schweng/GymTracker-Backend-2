b1814b3bdf2ede04e8e0213ceab180454743cb98
Paul Schweng <paul.schweng@siemens.com>
2023-06-06 23:06:15 +0200

    fixed cors

--- 0-cleanproject-plugins/src/main/java/de/dhbw/plugins/security/SecurityConfig.java
+++ 0-cleanproject-plugins/src/main/java/de/dhbw/plugins/security/SecurityConfig.java
@@ -4,19 +4,12 @@ import de.dhbw.cleanproject.domain.user.UserApplication;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.SecurityBuilder;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
@@ -29,16 +22,18 @@ public class SecurityConfig {
 
     private final UserApplication userApplication;
 
-
+/*
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost");
+                registry.addMapping("/**").allowedOrigins("http://localhost:8100");
             }
         };
     }
+
+ */
 
 
     @Bean
@@ -71,6 +66,7 @@ public class SecurityConfig {
         return http.build();
     }
 
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration configuration = new CorsConfiguration();
@@ -87,6 +83,8 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
+
 
 
 }
