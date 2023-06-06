5badb3734b974c7c95f09c05919a3586fdd2c9b7
Paul Schweng <paul.schweng@siemens.com>
2023-06-06 23:27:42 +0200

    fixed cors 2

--- 0-cleanproject-plugins/src/main/java/de/dhbw/plugins/security/SecurityConfig.java
+++ 0-cleanproject-plugins/src/main/java/de/dhbw/plugins/security/SecurityConfig.java
@@ -78,7 +78,7 @@ public class SecurityConfig {
         configuration.setAllowCredentials(true);
         // setAllowedHeaders is important! Without it, OPTIONS preflight request
         // will fail with 403 Invalid CORS request
-        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type", "X-Requested-With"));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
