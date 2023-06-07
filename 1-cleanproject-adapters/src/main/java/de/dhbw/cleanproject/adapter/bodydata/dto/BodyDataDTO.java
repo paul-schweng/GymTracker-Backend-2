6d27a4afe2a211da9147fe27c1608da49abc7ba3
Paul Schweng <paul.schweng@siemens.com>
2023-06-07 18:24:52 +0200

    fixed bodydata

--- 1-cleanproject-adapters/src/main/java/de/dhbw/cleanproject/adapter/bodydata/dto/BodyDataDTO.java
+++ 1-cleanproject-adapters/src/main/java/de/dhbw/cleanproject/adapter/bodydata/dto/BodyDataDTO.java
@@ -1,7 +1,9 @@
 package de.dhbw.cleanproject.adapter.bodydata.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -10,6 +12,8 @@ import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class BodyDataDTO {
     @NotNull
     private RightLeftDTO bicep, forearm, leg;
