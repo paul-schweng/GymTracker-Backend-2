73518e1a6bbeddd906584c2f9fa10054259d3d06
Paul Schweng <paul.schweng@siemens.com>
2023-06-07 19:16:51 +0200

    updated build

--- 0-cleanproject-plugins/src/main/java/de/dhbw/plugins/security/SecurityConfig.java
+++ 0-cleanproject-plugins/src/main/java/de/dhbw/plugins/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -17,7 +18,7 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     private final UserApplication userApplication;
@@ -36,15 +37,15 @@ public class SecurityConfig {
  */
 
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .cors()
                 .and()
-                .httpBasic()
-                .and()
                 .csrf()
                     .disable()
+                .httpBasic()
+                .and()
                 .authorizeRequests()
                     .antMatchers("/auth/register", "/auth/available").permitAll()
                     .anyRequest().authenticated()
@@ -63,7 +64,6 @@ public class SecurityConfig {
                     .key("AbcdefghiJklmNoPqRstUvXyz") // cookies will survive if restarted
                 ;
 
-        return http.build();
     }
 
 
@@ -71,8 +71,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost"));
-        configuration.setAllowedMethods(List.of("HEAD",
-                "GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
         // setAllowCredentials(true) is important, otherwise:
         // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
         configuration.setAllowCredentials(true);
